902f33d34a8e757f1d4e5510d07b9778715d3960
Aleksander <afranczak@edu.cdv.pl>
2020-12-08 13:56:37 +0000

    deleted images folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,13 +17,6 @@ module.exports = {
         failOnError: true,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
-      },
-    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     "gatsby-image",
