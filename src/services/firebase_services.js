1194c8ae978aba59f118f51b336cbcf13f6b27a6
Aleksander Franczak <a.franczak@codigee.com>
2023-10-09 17:15:35 +0200

    updated Firebase tokens

--- src/services/firebase_services.js
+++ src/services/firebase_services.js
@@ -3,16 +3,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-var firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAwzp6Zp3cV98EjVO81buDmIlwsn-JfL0M",
   authDomain: "primadent-d96de.firebaseapp.com",
-  databaseURL:
-    "https://primadent-d96de-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://primadent-d96de-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "primadent-d96de",
   storageBucket: "primadent-d96de.appspot.com",
   messagingSenderId: "375690770066",
   appId: "1:375690770066:web:ac1e660985944b14e8584b",
-  measurementId: "G-VCL1MJF0NR",
+  measurementId: "G-VCL1MJF0NR"
 };
 export let auth, firestore;
 if (!firebase.apps.length) {
